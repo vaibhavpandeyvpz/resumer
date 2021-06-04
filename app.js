a6a0d66f1b6d5de6dd2f160eceae0d089a097bbf
Vaibhav Pandey <contact@vaibhavpandey.com>
2021-06-05 00:06:39 +0530

    Good-bye cookies. Shit ain't needed.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -11,7 +10,6 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
