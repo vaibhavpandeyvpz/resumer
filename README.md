26b00109edfa555db72d0f6c92cfec701e80096a
Vaibhav Pandey <contact@vaibhavpandey.com>
2021-06-05 10:20:14 +0530

    Force editor config, create README.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Resumer
+
+Open-source theme-able (just 1 as of now, you can send pull requests for more) resume/cv builder.
+
+It is built using [React.js](https://reactjs.org/) + [Express](https://expressjs.com/) and runs on [Heroku](https://www.heroku.com/) platform.
+
+### Build
+
+To build & run the app locally, run below commands after cloning the project:
+
+```shell
+# get the dependencies
+yarn install
+
+# build from sources
+yarn run build
+
+# start the server
+yarn start
+```
+
+### License
+
+See the [LICENSE](LICENSE) file.
--- test/api.spec.js
+++ test/api.spec.js
@@ -3,11 +3,11 @@ const app = require('../app');
 
 describe('GET /api', function() {
 
-    it('responds home URL', done => {
-        request(app)
-            .get('/api')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200, { source: 'https://github.com/vaibhavpandeyvpz/resumer' }, done);
-    });
+  it('responds home URL', done => {
+    request(app)
+      .get('/api')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, { source: 'https://github.com/vaibhavpandeyvpz/resumer' }, done);
+  });
 });
