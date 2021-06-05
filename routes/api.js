a79c2514425f32d24b207120ed7d9a80ef61074c
Vaibhav Pandey <contact@vaibhavpandey.com>
2021-06-05 09:53:59 +0530

    Create tests for API.

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET the message. */
+/* GET the home. */
 router.get('/', (req, res, next) => {
-  res.send('https://github.com/vaibhavpandeyvpz/resumer');
+  res.send({ source: 'https://github.com/vaibhavpandeyvpz/resumer' });
 });
 
 module.exports = router;
---
+++ test/api.spec.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /api', function() {
+
+    it('responds home URL', done => {
+        request(app)
+            .get('/api')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, { source: 'https://github.com/vaibhavpandeyvpz/resumer' }, done);
+    });
+});
